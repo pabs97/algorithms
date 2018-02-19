41acfb54c06037e16814a5643e84ddcf78da8e5d
Pablo Suarez <pablo97suarez@gmail.com>
2018-02-18 18:35:51 -0800

    support for outputting arrays

--- addtwonumbers.js
+++ addtwonumbers.js
@@ -11,6 +11,12 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
+  // convert to linkedlist
+  if (Array.isArray(l1)) {
+    l1 = convertToLinkedList(l1);
+    l2 = convertToLinkedList(l2);
+  }
+
   let list1 = l1;
   let list2 = l2;
   let nextVal1 = list1.val || 0;
@@ -34,10 +40,18 @@ var addTwoNumbers = function (l1, l2) {
     nextVal1 = list1 && list1.val;
     nextVal2 = list2 && list2.val;
   }
-  return sl;
+
+  // return sl;
+  return linkedListToArray(sl);
 };
 
 let convertToLinkedList = (number) => {
+
+  // Supporting Array
+  if (Array.isArray(number)) {
+    number = number.join('');
+  }
+
   let str = number.toString();
   let ln = new ListNode(parseInt(str[str.length - 1]));
   let nn = ln;
@@ -47,4 +61,15 @@ let convertToLinkedList = (number) => {
     nn = nn.next = new ListNode(parseInt(str[i--]));
   }
   return ln;
-}
+};
+
+let linkedListToArray = ll => {
+  let arr = [];
+  arr.push(ll.val);
+
+  while (ll.next && ll.next.val) {
+    ll = ll.next;
+    arr.push(ll.val);
+  }
+  return arr;
+};
